8cdc7904dfa1344fefeeb0a25220dd73e2ce5482
Akshay Chaulya <akshaychaulya09@gmail.com>
2023-07-26 18:21:56 +0530

    Initial commit

---
+++ JS/logic.js
@@ -0,0 +1,514 @@
+// Welcom function logic
+const welcom = async function () {
+    let p1 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML = "W";
+            resolve(1)
+        }, 150)
+    })
+
+    let p2 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "e";
+            resolve(1)
+        }, 150)
+    })
+
+    let p3 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "l";
+            resolve(1)
+        }, 150)
+    })
+
+    let p4 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "l";
+            resolve(1)
+        }, 150)
+    })
+
+    let p5 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "c";
+            resolve(1)
+        }, 150)
+    })
+
+    let p6 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "o";
+            resolve(1)
+        }, 150)
+    })
+
+    let p7 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "m";
+            resolve(1)
+        }, 150)
+    })
+
+    let p8 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "e";
+            resolve(1)
+        }, 150)
+    })
+
+    let p9 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += " ";
+            resolve(1)
+        }, 150)
+    })
+
+    let p10 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "T";
+            resolve(1)
+        }, 150)
+    })
+
+    let p11 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "o";
+            resolve(1)
+        }, 150)
+    })
+
+    let p12 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += " ";
+            resolve(1)
+        }, 150)
+    })
+
+    let p13 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "C";
+            resolve(1)
+        }, 150)
+    })
+
+    let p14 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "o";
+            resolve(1)
+        }, 150)
+    })
+
+    let p15 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "d";
+            resolve(1)
+        }, 150)
+    })
+
+    let p16 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "i";
+            resolve(1)
+        }, 150)
+    })
+
+    let p17 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "n";
+            resolve(1)
+        }, 150)
+    })
+
+    let p18 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "g";
+            resolve(1)
+        }, 150)
+    })
+
+    let p19 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += " ";
+            resolve(1)
+        }, 150)
+    })
+
+    let p20 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "T";
+            resolve(1)
+        }, 150)
+    })
+
+    let p21 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "e";
+            resolve(1)
+        }, 150)
+    })
+
+    let p22 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "s";
+            resolve(1)
+        }, 150)
+    })
+
+    let p23 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "t";
+            resolve(1)
+        }, 150)
+    })
+
+    let p24 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "s";
+            resolve(1)
+        }, 150)
+    })
+
+    let p25 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += " ";
+            resolve(1)
+        }, 150)
+    })
+
+    let p26 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "D";
+            resolve(1)
+        }, 150)
+    })
+
+    let p27 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "e";
+            resolve(1)
+        }, 150)
+    })
+
+    let p28 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "t";
+            resolve(1)
+        }, 150)
+    })
+
+    let p29 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "a";
+            resolve(1)
+        }, 150)
+    })
+
+    let p30 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "i";
+            resolve(1)
+        }, 150)
+    })
+
+    let p31 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "l";
+            resolve(1)
+        }, 150)
+    })
+
+    let p32 = await new Promise((resolve, reject) => {
+        let T1 = document.getElementById("T1");
+        setTimeout(() => {
+            T1.innerHTML += "s";
+            resolve(1)
+        }, 150)
+    })
+}
+welcom();
+//----------------------------------------------------------------------------------
+
+// Bottam page controls functions
+const pageCount = () => {
+
+    let button = Array.from(document.getElementsByClassName("btn"))
+
+    let pageNo = 1;
+    let btnSuccess = button[6]
+    let lastCard = document.getElementById("last-card");
+    btnSuccess.onclick = () => {
+
+        hid.hidden = false;
+        pageNo = pageNo + 1;
+        button[5].innerHTML = pageNo;
+        if (pageNo >= 12) {
+            button[6].setAttribute('disabled', 'disabled')
+            lastCard.setAttribute('hidden', 'hidden');
+        }
+        else if (pageNo <= 1) {
+            button[4].setAttribute('disabled', 'disabled')
+        }
+        else {
+            button[4].removeAttribute('disabled', 'disabled')
+        }
+
+        addHTML(pageNo)
+        // changWidth();
+    }
+
+    let btnDanger = button[4];
+    btnDanger.onclick = () => {
+
+        pageNo = pageNo - 1;
+        button[5].innerHTML = pageNo;
+        lastCard.removeAttribute('hidden', 'hidden');
+        if (pageNo <= 1) {
+            button[4].setAttribute('disabled', 'disabled')
+        }
+        else {
+            button[6].removeAttribute('disabled', 'disabled')
+        }
+
+        addHTML(pageNo)
+
+    }
+}
+pageCount();
+
+//----------------------------------------------------------------------------------
+
+// facth API
+
+const thePromis = async () => {
+
+    let condText = await fetch("https://kontests.net/api/v1/all")
+    let value = await condText.json();
+    return value;
+
+}
+
+const getThePromis = async (a) => {
+    let mainValue = await thePromis();
+    // console.log(mainValue);
+    return mainValue[a];
+}
+
+//---------------------------------------------------------------------------------------
+// for searching
+
+const searchig = async () => {
+    let promiseValue = await thePromis();
+    let forSearch = document.querySelector(".forSearch");
+    let afterSearch = document.querySelector(".afterSearch");
+
+    let users = []
+    let searchInput = document.getElementsByClassName("form-control")[0];
+    searchInput.addEventListener("input", (e) => {
+
+        forSearch.classList.add('active1');
+
+        afterSearch.classList.add('active2')
+
+
+        let value = e.target.value.toLowerCase()
+        users.forEach(user => {
+            const isVisible = user.name.toLowerCase().includes(value) || user.end_time.toLowerCase().includes(value) || user.siteName.toLowerCase().includes(value)
+            if(isVisible) {
+                console.log(user)
+
+                afterSearch.innerHTML = `
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${user.name}</h5>
+                            <p class="card-text">
+                               Site Name: ${user.siteName} <br>
+                               Pogram End Time: ${user.end_time}
+                            </p>
+                            <a href="${user.url}" class="btn btn-primary">Link</a>
+                        </div>
+                    </div>
+                `
+
+            }
+
+            if (value === "") {
+                window.location.assign("index.html")
+            }
+
+        })
+
+    })
+
+    users = promiseValue.map(element => {
+        return { name: element.name, end_time: element.end_time, siteName: element.site, url: element.url }
+    });
+}
+searchig()
+
+//------------------------------------------------------------------------------
+
+// Add API value in Web site 
+
+const addHTML = async (c = 1) => {
+    let x;
+    let y;
+    let z;
+    switch (c) {
+        case 1:
+            x = 0;
+            y = 1;
+            z = 2;
+
+            // console.log(z)
+            break;
+        case 2:
+            x = 3;
+            y = 4;
+            z = 5;
+            break;
+        case 3:
+            x = 6;
+            y = 7;
+            z = 8;
+            break;
+        case 4:
+            x = 9;
+            y = 10;
+            z = 11;
+            break;
+        case 5:
+            x = 12;
+            y = 13;
+            z = 14;
+            break;
+        case 6:
+            x = 15;
+            y = 16;
+            z = 17;
+            break;
+        case 7:
+            x = 18;
+            y = 19;
+            z = 20;
+            break;
+        case 8:
+            x = 21;
+            y = 22;
+            z = 23;
+            break;
+        case 9:
+            x = 24;
+            y = 25;
+            z = 26;
+            break;
+        case 10:
+            x = 27;
+            y = 28;
+            z = 29;
+            break;
+        case 11:
+            x = 30;
+            y = 31;
+            z = 32;
+            break;
+        case 12:
+            x = 33;
+            y = 34;
+            break;
+    }
+
+    let theValue1 = await getThePromis(x);
+    let theValue2 = await getThePromis(y);
+    let theValue3 = await getThePromis(z);
+
+
+    let end_time = [theValue1.end_time, theValue2.end_time, theValue2.end_time];
+    let siteName = [theValue1.site, theValue2.site, theValue3.site];
+    let url = [theValue1.url, theValue2.url, theValue3.url];
+    let name = [theValue1.name, theValue2.name, theValue3.name];
+
+    let cardTitle = Array.from(document.getElementsByClassName("card-title"));
+    let cardText = Array.from(document.getElementsByClassName("card-text"))
+    let btnPrimary = Array.from(document.getElementsByClassName("btn-primary"));
+
+    function cardDetails1() {
+        cardTitle[0].innerHTML = name[0];
+        cardText[0].innerHTML = `Site Name: ${siteName[0]} <br> Pogram End Time: <br> ${end_time[0]}`
+        btnPrimary[0].href = url[0];
+    }
+
+    function cardDetails2() {
+        cardTitle[1].innerHTML = name[1];
+        cardText[1].innerHTML = `Site Name: ${siteName[1]} <br> Pogram End Time: <br> ${end_time[1]}`
+        btnPrimary[1].href = url[1];
+    }
+
+    function cardDetails3() {
+        cardTitle[2].innerHTML = name[2];
+        cardText[2].innerHTML = `Site Name: ${siteName[2]} <br> Pogram End Time: <br> ${end_time[2]}`
+        btnPrimary[2].href = url[2];
+    }
+    cardDetails1(name, end_time, siteName, url)
+    cardDetails2(name, end_time, siteName, url)
+    cardDetails3(name, end_time, siteName, url)
+
+
+}
+
+addHTML();
+
+//----------------------------------------------------------------------------
+
+// let con1 = confirm("Do you like to save a tittle and its value")
+// if (con1 == true) {
+//     let tittle = prompt("Enter the tittle").valueOf();
+//     let value = prompt("Enter the value").valueOf();
+//     function saveNote() {
+//         localStorage.setItem(`${tittle}`, `${value}`)
+//     }
+//     saveNote();
+// }
+
+// let con2 = confirm("Do you like to see this all savings value?");
+
+// if(con2 == true){
+//     const seenTheSaveNote = () => {
+//         alert(`Saving thing is: ${localStorage} `)
+//         console.log("this work")
+//     }
+
+//     seenTheSaveNote()
+
+// }
+
